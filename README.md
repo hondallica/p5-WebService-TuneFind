64454ad7b8ba8c82211db3d9d893c7d9232f7d38
Hondallica <hondallica@gmail.com>
2015-06-23 20:11:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::TuneFind - It's new $module
+
+# SYNOPSIS
+
+    use WebService::TuneFind;
+
+# DESCRIPTION
+
+WebService::TuneFind is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica <hondallica@gmail.com>
---
+++ lib/WebService/TuneFind.pm
@@ -0,0 +1,39 @@
+package WebService::TuneFind;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::TuneFind - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::TuneFind;
+
+=head1 DESCRIPTION
+
+WebService::TuneFind is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
